1d73caa99d66d85fb8d3edfa86cb382441fa84e2
Виталий Чухляев <vitaliy69@mail.ru>
2022-11-15 22:32:50 +0300

    Реализация класса Stack. Task 1

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+class Stack:
 
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self): #  проверка стека на пустоту
+        return self.items == []
+
+    def push(self, item): # добавляет новый элемент(последний)
+        self.items.append(item)
+
+    def pop(self): # удаляет верхний элемент стека(последний)Стек изменяется
+        return self.items.pop()
+
+    def peek(self): # возвращает верхний элемент стека, но не удаляет его. Стек не меняется
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items) #  возвращает количество элементов в стеке
 
 
 if __name__ == '__main__':
-    ...
-
-
+    a = Stack()
+    print(a)
+    print(a.__dict__)
